9800acc856c2a61c1ac9a0f2fc93caf5d0bbd011
Magdalena <mdeschner@hotmail.de>
2019-05-27 21:33:36 +0200

    add pagination to scraper

--- receipes/receipes/spiders/receipes_spyder.py
+++ receipes/receipes/spiders/receipes_spyder.py
@@ -11,6 +11,9 @@ class ReceipesSpyder(scrapy.Spider):
     # define start urls
     start_urls = ["https://www.chefkoch.de/rs/s0e1n1z1b0i0m100000/Rezepte.html"]
 
+    # define page number
+    page_number = 30
+
     def parse(self, response):
         """
         Parse html reponse of scraper.
@@ -24,13 +27,29 @@ class ReceipesSpyder(scrapy.Spider):
         # instantiate items
         items = ReceipesItem()
 
-        # get title, url
-        title = response.css(".ds-h3.ds-heading-link::text").extract()
-        url = response.css("body > main > article > a::attr(href)").extract()
+        # get all receipes 
+        all_receipes = response.css("body > main > article")
 
-        # store information as item
-        items["title"] = title
-        items["url"] = url
+        # iterate over receipes 
+        for receipe in all_receipes:
+            # get title and url
+            title = receipe.css("a::attr(data-vars-recipe-title)").extract()
+            url = receipe.css("a::attr(href)").extract()
 
-        # get items
-        yield items
+            # store title and url as item
+            items["title"] = title
+            items["url"] = url
+
+            # get items
+            yield items
+
+        # define url for next page
+        next_page = "https://www.chefkoch.de/rs/s"+ str(ReceipesSpyder.page_number) + "e1n1z1b0i0m100000/Rezepte.html"
+        
+        # check if next page number is below threshold
+        if ReceipesSpyder.page_number <= 100:
+            # increase page number by 30
+            ReceipesSpyder.page_number += 30
+
+            # get response of next page
+            yield response.follow(next_page, callback = self.parse)
