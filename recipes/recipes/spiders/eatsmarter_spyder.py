4299973ad85599fdfeba22184aeb76b2257d7619
Magdalena <mdeschner@hotmail.de>
2019-06-03 22:14:53 +0200

    adapt way of extracting ingredients

--- recipes/recipes/spiders/eatsmarter_spyder.py
+++ recipes/recipes/spiders/eatsmarter_spyder.py
@@ -1,4 +1,5 @@
 from ..items import RecipesItem
+import logging
 import os
 import re
 import scrapy
@@ -35,15 +36,18 @@ class EatsmarterSpyder(scrapy.Spider):
         for recipe in recipes:
             # extract information from html
             url = recipe.css("a::attr(href)").extract_first()
-            yield scrapy.Request("https://eatsmarter.de" + url, callback=self.parse_attr)
+            try:
+                yield scrapy.Request("https://eatsmarter.de" + url, callback=self.parse_attr)
+            except TypeError as e:
+                logging.info(e) 
 
         # define url for next page
         next_page = f"https://eatsmarter.de/suche/rezepte?page={EatsmarterSpyder.page_number}&ft=&op=Suchen&form_build_id=form-YbfzSni-wg3IicfsadcO_O9FSpmEEoQSfFhec4gsb94&form_id=eatsmarter_search_search_form"
         
         # check if next page number is below threshold
-        if ChefkochSpyder.page_number <= 10:
+        if EatsmarterSpyder.page_number <= 10:
             # increase page number by 1
-            ChefkochSpyder.page_number += 1
+            EatsmarterSpyder.page_number += 1
 
             # get response of next page
             yield response.follow(next_page, callback = self.parse)
@@ -61,16 +65,29 @@ class EatsmarterSpyder(scrapy.Spider):
         # initialize empty dict for ingredients
         ingredient_dict = {}
 
-        # get name of ingredient
-        ingredient_dict['name'] = response.css('a.name::text')
+        # get ingredients
+        # TODO: select container of ingredients
+        ingredients = response.css("#preparation .last")
+        ingredients_list = []
 
-        # get amount of ingredient
-        amount_amount = response.css('span.amount::text').extract()
-        amount_type = response.css('span.type::text').extract()
-        ingredient_dict["amount"] = 1 #" ".join(amount_amount, amount_type) 
+        for ingredient in ingredients:
+            # initialize empty list for ingredients
+            ingredient_dict = {}
+
+            # get name of ingredient
+            ingredient_dict['name'] = response.css('a.name::text').extract()
+
+            # get amount and type of ingredient
+            amount_type = response.css("span.amount::text").extract() +\
+                          response.css('span.type::text').extract()
+
+            ingredient_dict["amount"] =  amount_type
+
+            # append pair of ingredients to list of ingredients
+            ingredients_list.append(ingredient_dict)
 
         # get text
-        steps = " ".join(response.css("div.preparation-step-items p::text").extract())
+        text = " ".join(response.css("div.preparation-step-items p::text").extract())
 
         # store information as item
         items["title"] = title 
