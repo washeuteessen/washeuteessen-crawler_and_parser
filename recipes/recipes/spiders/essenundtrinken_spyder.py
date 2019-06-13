95dece276a8297a74f150603ad8349f3b90d0028
Magdalena <mdeschner@hotmail.de>
2019-06-13 22:01:42 +0200

    get domain, url and text from essenuntrinken recipes

--- recipes/recipes/spiders/essenundtrinken_spyder.py
+++ recipes/recipes/spiders/essenundtrinken_spyder.py
@@ -17,6 +17,7 @@ class ChefkochSpyder(CrawlSpider):
     start_urls = ["https://www.essen-und-trinken.de"]
 
     # define rule to only parse links with "rezepte"
+    #TODO: expand rule to follow more than one depth
     rules = (Rule(LxmlLinkExtractor(allow="/rezepte/[0-9]{5}-*"), callback="parse_item"),)
 
     def parse_item(self, response):
@@ -35,16 +36,24 @@ class ChefkochSpyder(CrawlSpider):
         # get recipe title
         title = response.css(".headline-title::text").extract_first()
 
+        # TODO: check why some times image source is not found
         # get title picture
         img_src = response.css(".recipe-img > img:nth-child(1)::attr(src)").extract_first()
+
+        # get ingredients
+        # (".ingredients-list li")
+
+        # get text
+        #TODO: check why some texts are not scraped
+        text = " ".join(response.css("ul.preparation li.preparation-step div.preparation-text p::text").extract())
 
         # store information as item
         items["title"] = title 
         items["domain"] = self.name
         items["img_src"] = img_src
         # items["ingredients"] = ingredients_list
-        # items["url"] = response.url
-        # items["text"] = text
+        items["url"] = response.url
+        items["text"] = text
 
         return items
 
