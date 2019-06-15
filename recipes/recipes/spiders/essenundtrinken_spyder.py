492e56540481bdb64be21735348338303b1e077b
Magdalena <mdeschner@hotmail.de>
2019-06-15 13:46:37 +0200

    adapt spider to crawl all internal links but only store recipe urls content

--- recipes/recipes/spiders/essenundtrinken_spyder.py
+++ recipes/recipes/spiders/essenundtrinken_spyder.py
@@ -16,8 +16,8 @@ class ChefkochSpyder(CrawlSpider):
     # define start urls
     start_urls = ["https://www.essen-und-trinken.de"]
 
-    # define rule to only parse links with "rezepte"
-    rules = (Rule(LxmlLinkExtractor(allow="/rezepte/[0-9]{5}-*", deny=".jpg"), callback="parse_item", follow=True),)
+    # define rule to only parse internal links 
+    rules = (Rule(LxmlLinkExtractor(allow_domains="essen-und-trinken.de"), callback="parse_item", follow=True),)
 
     def parse_item(self, response):
         """
@@ -32,39 +32,46 @@ class ChefkochSpyder(CrawlSpider):
         # instantiate items
         items = RecipesItem()
 
-        # get recipe title
-        title = response.css(".headline-title::text").extract_first()
+        # check if url contains a recipe
+        if re.search(pattern="/rezepte/[0-9]{5}-", string=response.url) is not None and re.search(pattern=".jpg", string=response.url) is None:
 
-        # get title picture
-        img_src = response.css(".recipe-img > img:nth-child(1)::attr(src)").extract_first()
+            # get recipe title
+            title = response.css(".headline-title::text").extract_first()
+            title = title.strip()
 
-        # get ingredients
-        ingredients = response.css("ul.ingredients-list li::text").extract()
+            # get title picture
+            img_src = response.css(".recipe-img > img:nth-child(1)::attr(src)").extract_first()
 
-        # strip \n
-        ingredients = [re.sub("\n", "", ingredient) for ingredient in ingredients]
+            # get ingredients
+            ingredients = response.css("ul.ingredients-list li::text").extract()
 
-        # strip whitespace
-        ingredients = [re.sub(' +', " ", ingredient) for ingredient in ingredients]
-        ingredients = [ingredient.strip() for ingredient in ingredients]
+            # strip \n
+            ingredients = [re.sub("\n", "", ingredient) for ingredient in ingredients]
 
-        # strip empty list elemens
-        ingredients = [ingredient for ingredient in ingredients if len(ingredient)>0]
+            # strip whitespace
+            ingredients = [re.sub(' +', " ", ingredient) for ingredient in ingredients]
+            ingredients = [ingredient.strip() for ingredient in ingredients]
 
-        # get text
-        text = " ".join(response.css("ul.preparation li.preparation-step div.preparation-text p::text").extract())
+            # strip empty list elemens
+            ingredients = [ingredient for ingredient in ingredients if len(ingredient)>0]
 
-        # sometimes text is not within paragraph
-        if len(text)<1:
-            text = " ".join(response.css("ul.preparation li.preparation-step div.preparation-text::text").extract())
+            # get text
+            text = " ".join(response.css("ul.preparation li.preparation-step div.preparation-text p::text").extract())
 
-        # store information as item
-        items["title"] = title 
-        items["domain"] = self.name
-        items["img_src"] = img_src
-        items["ingredients"] = ingredients
-        items["url"] = response.url
-        items["text"] = text
+            # sometimes text is not within paragraph
+            if len(text)<1:
+                text = " ".join(response.css("ul.preparation li.preparation-step div.preparation-text::text").extract())
+
+            # store information as item
+            items["title"] = title 
+            items["domain"] = self.name
+            items["img_src"] = img_src
+            items["ingredients"] = ingredients
+            items["url"] = response.url
+            items["text"] = text
+
+        else:
+            pass
 
         return items
 
